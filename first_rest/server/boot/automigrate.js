4f2cf02df473f2107d02858d5f74cda8611b0a70
Priyanshu Kumar <priyanshu966106@gmail.com>
2018-06-20 18:32:07 +0530

    ACL

--- first_rest/server/boot/automigrate.js
+++ first_rest/server/boot/automigrate.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports=function(app){
-    app.dataSources.mysql.automigrate(['company','account','product','category'],err=>{
+    app.dataSources.mysql.automigrate(['company','account','product','category','ACL','AccessToken'],err=>{
         if(err) throw err;
         console.log("moved table");
     })
