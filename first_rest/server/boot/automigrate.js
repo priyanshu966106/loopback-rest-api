5bda1a960102419b7c31bf9eec625eea05d34864
Priyanshu Kumar <priyanshu966106@gmail.com>
2018-06-20 15:43:26 +0530

    models

--- first_rest/server/boot/automigrate.js
+++ first_rest/server/boot/automigrate.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports=function(app){
-    app.dataSources.mysql.automigrate('company',err=>{
+    app.dataSources.mysql.automigrate(['company','account','product','category'],err=>{
         if(err) throw err;
         console.log("moved table");
     })
