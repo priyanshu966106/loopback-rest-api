4e15ce31d61c6f3769cd0f0e6a2f5de45404124b
Priyanshu Kumar <priyanshu966106@gmail.com>
2018-06-27 09:58:13 +0530

    container

--- first_rest/common/models/account.js
+++ first_rest/common/models/account.js
@@ -1,5 +1,7 @@
 'use strict';
-
 module.exports = function(Account) {
+    
+    Account.validatesLengthOf('accno', {min: 3, message: {min: 'Password is too short'}});
 
 };
+
--- first_rest/common/models/product.js
+++ first_rest/common/models/product.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(Product) {
-
+ Product.validatesLengthOf('pname', {min: 3, message: {min: 'Password is too short'}});
 };
